6a64fadc87a6292107d81144f0d6b5c0c440a528
Gaurav <97354276+Gaurav-710@users.noreply.github.com>
2022-11-17 13:06:24 +0530

    main

---
+++ FCFS.java
@@ -0,0 +1,133 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class FCFS {
+
+	
+
+	public static void main(String Args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		 System.out.println("Enter Number of Pages:");
+		 int numPages=sc.nextInt();
+		 int pages[]=new int[numPages];
+		 System.out.println("Enter Reference String: ");
+		 for(int i=0;i<numPages;i++)
+		 {
+			 pages[i]=sc.nextInt();
+		 }
+		 
+		 System.out.println("Enter Number of Frames");
+		 int capacity=sc.nextInt();
+		 
+		 //To represent set of current pages
+		 HashSet<Integer> frames=new HashSet<>(capacity);
+		 
+		 //To store pages o=in FIFO manner
+		 Queue<Integer> index=new LinkedList<>();
+		 int pageFaults=0;
+		 int hits=0;
+		 for(int i=0;i<numPages;i++)
+		 {
+			 if(frames.size()<capacity) //check if set can hold n=more pages
+			 {
+				 //IF page not present insert into set and increment pagefault
+				 if(!frames.contains(pages[i])) 
+				 {
+					 frames.add(pages[i]);
+					 index.add(pages[i]); //push current page into queue
+					 pageFaults++;
+					// System.out.println(pageFaults);
+					// frames.forEach(System.out::print); 
+					 for(int j:index)
+						 System.out.print(j+"\t");
+					 System.out.println();
+				 }
+				 else
+				 {
+					 hits++;
+				 }
+			 }
+			 else //set is full,need replacement
+			 {
+				 if(!frames.contains(pages[i])) //frame is not present present
+				 {
+					 int val=index.peek();
+					 index.poll();
+					 frames.remove(val);
+					 
+					 frames.add(pages[i]);
+					 index.add(pages[i]);
+					 pageFaults++;
+					 for(int j:index)
+						 System.out.print(j+"\t");
+					 System.out.println();
+				 }
+				 else //frame is present in set
+				 {
+					 hits++;
+				 }
+			 }
+		 }
+		 
+		 System.out.println("Number of Page Faults : "+pageFaults);
+		 System.out.println("Hits:\t"+hits);
+		 System.out.println("hit ratio: "+((double)hits/(double)numPages));
+
+	}
+}
+
+Output:
+/*
+Enter Number of Pages:
+12
+2 3 2 1 5 2 4 5 3 2 5 2
+Enter Number of Frames
+3
+1
+2
+3
+4
+5
+6
+7
+8
+9
+Number of Page Faults : 9
+Hits:	3
+hit ratio: 0.25
+*/
+
+K:\sposl\PageReplacement\src>java FCFS
+Enter Number of Pages:
+4
+Enter Reference String:
+1 2 3 4
+Enter Number of Frames
+4
+1
+1       2
+1       2       3
+1       2       3       4
+Number of Page Faults : 4
+Hits:   0
+hit ratio: 0.0
+
+K:\sposl\PageReplacement\src>java FCFS
+Enter Number of Pages:
+5
+Enter Reference String:
+1 2 3 1 4
+Enter Number of Frames
+3
+1
+1       2
+1       2       3
+2       3       4
+Number of Page Faults : 4
+Hits:   1
+hit ratio: 0.2
+
+K:\sposl\PageReplacement\src>
